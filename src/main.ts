0be62b785e1d316300fffc42f83abeaf21699acc
saranjith <saranjith@metrictreelabs.com>
2023-03-16 11:44:42 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,11 @@
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import serverless = require('serverless-http');
 const port = parseInt(process.env.APP_PORT);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = '.netlify/functions/main';
-  app.setGlobalPrefix(globalPrefix);
-  await app.init();
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverless(expressApp)
-  // await app.listen(port, () => {
-  //   Logger.log(`Let's talk ${port}`);
-  // });
+  await app.listen(port, () => {
+    Logger.log(`Let's talk ${port}`);
+  });
 }
-let server;
-export const handler = async (event, context, callback) => {
-  server = server ?? (await bootstrap());
-  return server(event, context, callback);
-}
+bootstrap();
