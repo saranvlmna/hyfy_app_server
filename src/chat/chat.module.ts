d0b936be46805600aff92493276cf21fbe59c98b
Saranjith <saranjith@metrictreelabs.com>
2023-07-22 14:36:35 +0530

    update

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Active, ActiveSchema } from "../shared/database/activeusers";
 import { ChatService } from "./chat.service";
-import { Message, MessageSchema } from "src/shared/database/messages";
+import { Message, MessageSchema } from "../shared/database/messages";
 
 @Module({
   imports: [
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Active } from "../shared/database/activeusers";
-import { Message } from "src/shared/database/messages";
+import { Message } from "../shared/database/messages";
 @Injectable()
 export class ChatService {
   constructor(
