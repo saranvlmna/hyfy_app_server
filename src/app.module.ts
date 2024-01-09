5a500f339f0c24391c7448a331ddb0ab116e1b0a
Saranjith <saranjith@metrictreelabs.com>
2024-01-09 15:00:32 +0530

    merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
+import { hyFyController } from "./app.controller";
 import { AuthModule } from "./auth/auth.module";
 import { ChatModule } from "./chat/chat.module";
 import { ChatGateway } from "./chat/chatRoom";
 import { personalChat } from "./chat/personalChat";
 import { CommunicationModule } from "./communication/communication.module";
-import { hyFyController } from "./app.controller";
 import { UserModule } from "./user/user.module";
-import { hyFyController } from "./app.controller";
 @Module({
   imports: [
     ConfigModule.forRoot(),
