1db7f43fe153538840706b9d69345272a5bee6ac
Saranjith <saranjith@metrictreelabs.com>
2024-01-09 15:08:07 +0530

    debug

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { BadGatewayException, Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
-import verification from "src/shared/constants/verification";
+import verification from "../shared/constants/verification";
 import { CommunicationService } from "../communication/communication.service";
 import { Otp } from "../shared/database/otp";
 import { Users } from "../shared/database/users";
