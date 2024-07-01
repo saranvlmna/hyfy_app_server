6e621841a10562b707d7be5aaa99769635bdacce
Saranjith <saranjith@metrictreelabs.com>
2024-07-01 16:14:50 +0530

    chat

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Active } from "../shared/database/activeuser";
-import { Message } from "../shared/database/message";
+import { Message } from "../shared/database/conversation";
 @Injectable()
 export class ChatService {
   constructor(
